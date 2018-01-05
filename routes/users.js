7c86f62d78e0c1ec6bcbb286f82265189c762c16
William Huynh <31399549+wdh7@users.noreply.github.com>
2018-01-04 18:48:04 -0800

    Add profile editing

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
-const { User, Profile, Location } = models;
+const { User, Profile, Location, sequelize } = models;
+
+router.get('/:id/edit', (req, res) => {
+  const userId = parseInt(req.params.id);
+
+  if (userId === req.session.currentUser.id) {
+    User.findById(userId, {
+      include: [
+        {
+          model: Profile,
+          include: [{model: Location}]
+        }
+      ]
+    })
+      .then(user => {
+        res.render('users/edit', {user});
+      })
+      .catch(err => {
+        res.status(500).send(err.stack);
+      });
+  } else {
+    res.redirect('/search');
+  }
+});
 
 router.get('/:id', (req, res) => {
   const userId = req.params.id;
@@ -20,6 +43,56 @@ router.get('/:id', (req, res) => {
     .catch(err => {
       res.status(500).send(err.stack);
     });
+});
+
+router.put('/:id', (req, res) => {
+  const userId = req.params.id;
+  const { username, age, city, about, talents, favThings, whyMe} = req.body;
+  let locationId;
+
+  sequelize.transaction(t => {
+    return User.update({
+      username: username
+    }, {
+      where: {
+        id: userId
+      },
+      transaction: t
+    }).then(() => {
+      return Profile.findOrCreate({
+        default: {age, about, talents, favThings, whyMe},
+        where: {id: userId},
+        transaction: t
+      })
+    }).spread(profile => {
+      locationId = profile.locationId;
+
+      return Profile.update({
+        age,
+        about,
+        talents,
+        favThings,
+        whyMe
+      }, {
+        where: {id: profile.id},
+        transaction: t
+      })
+    })
+      .then(() => {
+        return Location.update({
+          city
+        }, {
+          where: {id: locationId},
+          transaction: t
+        })
+      })
+      .then(() => {
+        res.redirect(`/users/${userId}`);
+      })
+      .catch(err => {
+        res.status(500).send(err.stack);
+      })
+  });
 });
 
 
